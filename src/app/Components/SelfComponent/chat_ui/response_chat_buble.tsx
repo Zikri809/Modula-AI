8dfe3c861d5985da25824ec4312752f5c8e34a8b
Zikri809 <mohdzikri809@gmail.com>
2025-09-28 19:47:17 +0800

    Added code block copying capability
    The code block now have functional copy button according to the format of the code

--- src/app/Components/SelfComponent/chat_ui/response_chat_buble.tsx
+++ src/app/Components/SelfComponent/chat_ui/response_chat_buble.tsx
@@ -7,12 +7,39 @@ import remarkMath from "remark-math";
 import Llm_waiting_animation from "@/app/Components/SelfComponent/chat_ui/Llm_waiting_animation";
 import {model_combo_box_list} from "@/app/Components/SelfComponent/chat_ui/llm_array";
 import {CopyIcon} from "lucide-react";
+import {toast} from "sonner";
+import 'prismjs/themes/prism-dark.css';
+import Prism from 'prismjs';
+import {useEffect} from "react";
 
+/*
+* syntax highlightinh isnt feasible due to technical confilict using markdown renderer
+* */
 
 
 
 export default function response_chat_buble({llm_model, llm_response, time, state}: Response_message_bubble) {
 
+    useEffect(() => {
+        console.log('ran ran ran');
+        Prism.highlightAll();
+    }, [llm_response]);
+    async function copyCodeToClipboard(text:string | undefined) {
+        //alert(object)
+        if(!text) {
+            toast.error('No text to copy to clipboard');
+            return
+        }
+        try{
+            console.log('react node data ',text)
+
+            await navigator.clipboard.writeText(text);
+            toast.success("Copied to clipboard!");
+        }
+        catch(err){
+            toast.error('Failed to copy code to clipboard! Try checking the permission for this.');
+        }
+    }
     return(
         <div className='text-sm sm:text-base flex flex-col gap-2  bg-black broder-b-1 border-neutral-500 h-fit w-full text-white'>
             <p className='pr-1 text-left bg-blcak text-white flex flex-row gap-2 items-center'>
@@ -40,13 +67,16 @@ export default function response_chat_buble({llm_model, llm_response, time, stat
                             <td className="px-4 py-2 border" {...props} />
                         ),
                         pre: ({node, ...props}) => (
-                            <div className={'!mr-0 rounded-md'}>
-                                <div className={'rounded-md p-4 !pb-0 bg-black right-4 top-4 w-full flex flex-row justify-between items-center'}>
+                            <div className={'!mr-0 rounded-md my-2'}>
+                                <div className={'rounded-t-md rounded-b-none m-0 p-4 !pb-0 bg-black right-4 top-4 w-full flex flex-row justify-between items-center'}>
                                     <p className={'font-bold'}>Code</p>
-                                    <button className={'flex flex-row gap-2 items-center text-white font-bold '}><CopyIcon size={15}/> Copy</button>
+                                    {//@ts-ignore
+                                        <button onClick={() => copyCodeToClipboard(node?.children[0].children[0].value)}
+                                             className={'cursor-pointer hover:text-neutral-300 flex flex-row gap-2 items-center text-white font-bold '}>
+                                        <CopyIcon size={15}/> Copy</button>}
                                 </div>
-                                <pre  className=' !mr-0 overflow-x-auto thin-scrollbar bg-black p-4 rounded-md'>
-                                  <code {...props}/>
+                                <pre  className='!m-0 !rounded-t-none !border-none !mr-0 overflow-x-auto thin-scrollbar !bg-black p-4 rounded-md'>
+                                  {props.children}
                                 </pre>
 
                             </div>
